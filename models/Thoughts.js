1ab843d4d41db62b9ca2534f08c71105203bb291
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-07-26 21:25:56 -0700

    added thought Schema

---
+++ models/Thoughts.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reactions');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+
+    },
+    {
+        toJSON: {
+            getters:true
+        },
+    }
+);
+
+const Thoughts = model('thoughts', thoughtSchema);
+
+module.exports = Thoughts;

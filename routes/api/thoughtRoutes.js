42a2a5ff284320b1b01745f605d478e10cfbb863
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-07-27 19:33:02 -0700

    added general routes for api, user, and thoughts

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    removeThought,
+    createThought,
+    updateThought,
+    addReaction,
+    removeReaction,
+} = require ('../../controllers/thoughtController');

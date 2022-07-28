0d4ccad664c6b06cacf109520c9e52fce1831029
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-07-27 19:49:21 -0700

    added thoughts routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,3 +9,21 @@ const {
     removeReaction,
 } = require ('../../controllers/thoughtController');
 
+// /api/thoughts/
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtsId
+router
+    .route('/:thoughtsId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(removeThought);
+
+
+// /api/thoughts/:thoughtId/reactions   
+router
+.route('/:thoughtsId/reactions')
+.post(addReaction)
+.delete(removeReaction);
+
+module.exports = router;
